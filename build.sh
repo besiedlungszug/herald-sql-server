7511a9416165ad0177500b62bd024974ed2455c3
DavidLokison <contact@davidlokison.com>
2026-05-05 16:08:31 +0200

    feat: add release docker workflow

--- build.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env zsh
-DOLT_VERSION=$(cat opt/DOLT_VERSION)
-while read -r HERALD_VERSION; do
-    podman build -t besiedlungszug/herald-sql-server:$HERALD_VERSION --build-arg DOLT_VERSION=$DOLT_VERSION --build-arg HERALD_VERSION=$HERALD_VERSION src
-done < <(curl https://www.dolthub.com/api/v1alpha1/besiedlungszug/herald/branches | jq -r '.branches[] | .branch_name' | grep 'release/' | awk -F'/' '{print $2}')
